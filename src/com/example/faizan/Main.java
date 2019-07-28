2439dba6dfbc7b8920fa989101daeceb8941e3b0
RazaTheDragon <faizan.white110@gmail.com>
2019-07-28 11:36:37 +0500

    made table class

--- src/com/example/faizan/Main.java
+++ src/com/example/faizan/Main.java
@@ -7,32 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         // What's wrong?
-        class Table {
-            public void table() {
-           //every thing is fine here_______
-           //                               |
-           //                               V
-           //___________________________________________________________________________________________________________
-                Scanner S = new Scanner(System.in);
-                System.out.println("Table of number ?");
-               int a = S.nextInt();
-                System.out.println("From ?");
-               int b = S.nextInt();
-                System.out.println("To ?");
-               int c = S.nextInt();
-                int d = b;
-                int e;
-                while (d <= c) {
-                    e = a * b;
-                    System.out.println(e);
-                    d++;
-                    b++;
-           //___________________________________________________________________________________________________________
-                }
-            }
-
-            Table(4,5,5);
-
-        }
+        Table table=new Table();
+        table.table();
     }
 }
